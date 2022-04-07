1664d45b00e4caf21d35b2d2715c1f8f5392b8f0
zanuar35 <rahmatzanuar35@gmail.com>
2022-04-07 09:04:52 +0700

    add update password

--- lib/app/modules/update_password/controllers/update_password_controller.dart
+++ lib/app/modules/update_password/controllers/update_password_controller.dart
@@ -1,3 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class UpdatePasswordController extends GetxController {}
+class UpdatePasswordController extends GetxController {
+  TextEditingController currC = TextEditingController();
+  TextEditingController newC = TextEditingController();
+  TextEditingController confirmC = TextEditingController();
+  RxBool isLoading = false.obs;
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  void updatePass() async {
+    if (currC.text.isNotEmpty &&
+        newC.text.isNotEmpty &&
+        confirmC.text.isNotEmpty) {
+      if (newC.text == confirmC.text) {
+        isLoading.value = true;
+        try {
+          // Update password
+          String emailUser = auth.currentUser!.email!;
+
+          await auth.signInWithEmailAndPassword(
+              email: emailUser, password: currC.text);
+
+          await auth.currentUser!.updatePassword(newC.text);
+
+          Get.back();
+          Get.snackbar("Berhasil", "Password berhasil diubah",
+              snackPosition: SnackPosition.TOP,
+              backgroundColor: Color.fromARGB(255, 88, 88, 88),
+              colorText: Colors.white);
+        } on FirebaseAuthException catch (e) {
+          if (e.code == 'wrong-password') {
+            Get.snackbar('Error', 'Wrong Password',
+                snackPosition: SnackPosition.BOTTOM,
+                backgroundColor: Color.fromARGB(255, 137, 137, 137),
+                colorText: Colors.white);
+          } else {
+            Get.snackbar("Kesalahan", e.code);
+          }
+        } catch (e) {
+          Get.snackbar("Kesalahan", "Update password gagal",
+              snackPosition: SnackPosition.BOTTOM,
+              backgroundColor: Color.fromARGB(255, 137, 137, 137),
+              colorText: Colors.white);
+        } finally {
+          isLoading.value = false;
+        }
+      } else {
+        Get.snackbar("Kesalahan", "Confirm password tidak cocok",
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Color.fromARGB(255, 137, 137, 137),
+            colorText: Colors.white);
+      }
+    } else {
+      Get.snackbar("Kesalahan", 'Input harus diisi',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Color.fromARGB(255, 137, 137, 137),
+          colorText: Colors.white);
+    }
+  }
+}
--- lib/app/modules/update_password/views/update_password_view.dart
+++ lib/app/modules/update_password/views/update_password_view.dart
@@ -8,16 +8,64 @@ class UpdatePasswordView extends GetView<UpdatePasswordController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('UpdatePasswordView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'UpdatePasswordView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: Text('Update Password'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: ListView(
+          padding: EdgeInsets.all(20),
+          children: [
+            TextField(
+              controller: controller.currC,
+              autocorrect: false,
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Current Password',
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              autocorrect: false,
+              controller: controller.newC,
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'New Password',
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              autocorrect: false,
+              obscureText: true,
+              controller: controller.confirmC,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Confirm New Password',
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Obx(
+              () => ElevatedButton(
+                onPressed: () {
+                  if (controller.isLoading.isFalse) {
+                    controller.updatePass();
+                  }
+                },
+                child: Text(
+                  controller.isLoading.isFalse
+                      ? 'Update Password'
+                      : 'Loading...',
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
