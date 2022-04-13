1f273c12a606d00c275921bcf47e3e3cb6b5c83c
zanuar35 <rahmatzanuar35@gmail.com>
2022-04-13 19:20:33 +0700

    update profile pict

--- lib/app/modules/update_profile/controllers/update_profile_controller.dart
+++ lib/app/modules/update_profile/controllers/update_profile_controller.dart
@@ -1,12 +1,17 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as s;
 
 class UpdateProfileController extends GetxController {
   TextEditingController nipC = TextEditingController();
   TextEditingController nameC = TextEditingController();
   TextEditingController emailC = TextEditingController();
+
+  s.FirebaseStorage storage = s.FirebaseStorage.instance;
 
   RxBool isLoading = false.obs;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -31,7 +36,20 @@ class UpdateProfileController extends GetxController {
         emailC.text.isNotEmpty) {
       isLoading.value = true;
       try {
-        firestore.collection("pegawai").doc(uid).update({"name": nameC.text});
+        Map<String, dynamic> data = {
+          "name": nameC.text,
+        };
+        if (image != null) {
+          File file = File(image!.path);
+          String ext = image!.name.split(".").last;
+
+          await storage.ref('$uid/profile.$ext').putFile(file);
+          String urlImage =
+              await storage.ref('$uid/profile.$ext').getDownloadURL();
+
+          data.addAll({"profile": urlImage});
+        }
+        await firestore.collection("pegawai").doc(uid).update(data);
         Get.snackbar("Success", "Berhasil Update profile");
       } catch (e) {
         Get.snackbar("Terjadi Kesalahan", 'tidak dapat update profile');
